4f86b502959e52de6396a503a792a210f2c9d710
pythy <the.pythy@gmail.com>
2008-11-06 12:42:36 +0600

    Add hg_tiprev_keyword monkey patch for hgext.keyword

---
+++ hg_tiprev_keyword/tiprev_keyword.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""
+Monkey patch for hgext.keyword
+for showing tiprev keyword
+"""
+
+import hgext.keyword
+import mercurial.templatefilters
+
+def get_tip_rev(rep):
+    chlog = rep.changelog
+    return chlog.count() - 1
+
+def patched_substitute(self, data, path, node, subfunc):
+    def kwsub(match):
+        kw = match.group(1)
+        if kw == 'tiprev':
+            tiprev = get_tip_rev(self.repo)
+            ekw = str(tiprev)
+        else:
+            # original kwsub
+            self.ct.use_template(self.templates[kw])
+            self.ui.pushbuffer()
+            self.ct.show(changenode=node, 
+                         root=self.repo.root,
+                         file=path)
+            ekw = mercurial.templatefilters.firstline(self.ui.popbuffer())
+        return '$%s: %s $' % (kw, ekw)
+    return subfunc(kwsub, data)
+
+if not getattr(hgext.keyword.kwtemplater, '_orig_substitute', False):
+    # here we go, gorilllaz, yeeeeeep
+    hgext.keyword.kwtemplater._orig_substitute = hgext.keyword.kwtemplater.substitute
+    hgext.keyword.kwtemplater.substitute = patched_substitute
